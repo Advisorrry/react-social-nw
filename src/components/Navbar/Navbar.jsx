70a0c5b48842c9ea888f3102ce674c880088bc21
Advisorrry <sypikeh@yandex.com>
2021-03-16 18:01:44 +0500

    рефакторинг

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import './navbar.scss'
-import {
-   
-    Link
-  } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 export function Navbar() {
     return (
-        <div className='navbar'>
+        <div className="navbar">
             <ul className="navbar__lists">
-                <Link to='/profile'><li>Профиль</li></Link>
-                <Link to='/messenger'><li>Мессенджер</li></Link>
-                <Link to='/users'><li>Пользователи</li></Link>
-                <Link to='/music'><li>Музыка</li></Link>
-                <Link to='/settings'><li>Настройки</li></Link>
-                
+                <Link to="/profile">
+                    <li>Профиль</li>
+                </Link>
+                <Link to="/messenger">
+                    <li>Мессенджер</li>
+                </Link>
+                <Link to="/users">
+                    <li>Пользователи</li>
+                </Link>
+                <Link to="/music">
+                    <li>Музыка</li>
+                </Link>
+                <Link to="/settings">
+                    <li>Настройки</li>
+                </Link>
             </ul>
         </div>
     )
